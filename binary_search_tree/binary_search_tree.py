e003f72e850bc4ac655d719a3a1b0fce2d7ab660
Julie Thrall <juliethrallstewart@gmail.com>
2019-12-19 22:43:26 -0800

    progress in binary_search_tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -54,6 +54,8 @@ class BinarySearchTree:
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
+
+    #for each is an example of a depth first traversal
     def for_each(self, cb):
         cb(self.value)
         if self.right is not None:
@@ -61,22 +63,124 @@ class BinarySearchTree:
         if self.left is not None:
             self.left.for_each(cb)
 
+    # iterative for_each:
+
+    def for_each_iterative(self,cb):
+        stack = Stack()
+        stack.push(self) #self equals the root node
+
+        while stack.len() > 0:
+            current_node = stack.pop()
+            if current_node.right:
+                stack.push(current_node.right)
+            if current_node.left:
+                stack.push(current_node.left)
+            cb(current_node.value)
+
+
     # DAY 2 Project -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
-    def in_order_print(self, node):
-        pass
+
+    # steps:
+    # make a stack
+    # put root in Stack
+    # while stack is not empty
+    #  pop the root out of stack = does the current value == the value we are looking for? (if target == value: return)
+    #  DO THE THING TO IT!!!!
+    #     if left, 
+    #         add left to stack
+    #     if right, 
+    #         add right to stack
+    def print(self, arr):
+        for i in arr:
+            print(i)
+   
+    def in_order_print(self, node): #this is a specific vocabulary term = research
+        res = []
+        if node:
+            res = self.in_order_print(node.left)
+            print(res, 'left')
+            res.append(node.value)
+            res = res + self.in_order_print(node.right)
+            print(res, 'right')
+           
+        return res
+
+        
+        
+            
+            
+            
+            
+       
+        # while res.size != 0:
+        #     v = res.dequeue()
+        #     print(v)
+        #     print(res.len())
+            # self.in_order_print(node)
+            # print(res.storage, "current value in storage cache")
+                
+                   
+        # if self.left is not None:
+        #     print(self.value)
+        #     # self.left.in_order_print(print(self.value))
+        #     # return True
+        # else:
+        #     pass
+            
+        # if self:
+        #     print(self.value)
+        #     self.left.in_order_print(node)
+        #     print(self.value)
+        #     self.right.in_order_print(node)
+           
+        # queue = Queue()
+        # root_node = self
+        # queue.enqueue(root_node)
+        # while queue.size > 0:
+       
+        #     if root_node.left is not None:            
+        #         print(root_node.left.value, "left")
+        #         root_node.left.in_order_print(node)
+        #     if root_node.right is not None:
+        #         print(root_node.right.value, "right")
+        #         root_node.right.in_order_print(node)
+        
+       
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
+        queue = Queue()
+        root_node = self
+        queue.enqueue(root_node)
+        while queue.size > 0:
+            current_root = queue.dequeue()
+            print(current_root.value)
+            if current_root.left is not None:
+                queue.enqueue(current_root.left)
+            if current_root.right is not None:
+                queue.enqueue(current_root.right)
+    
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        stack = Stack()
+        root_node = self
+        stack.push(root_node)
+        while stack.size > 0:
+            current_root = stack.pop()
+            print(current_root.value)
+            if current_root.left is not None:
+                stack.push(current_root.left)
+            if current_root.right is not None:
+                stack.push(current_root.right)
+
+
+        
 
     # STRETCH Goals -------------------------
     # Note: Research may be required
@@ -89,6 +193,18 @@ class BinarySearchTree:
     def post_order_dft(self, node):
         pass
 
+bst = BinarySearchTree(1)
+bst.insert(8)
+bst.insert(5)
+bst.insert(7)
+bst.insert(6)
+bst.insert(3)
+bst.insert(4)
+bst.insert(2)
+print(bst.dft_print(1), "dft_print")
+print(bst.bft_print(1), "bft_print")
+print(bst.in_order_print(bst), "in order print")
+
 # .. the BinarySearchTree is initialized with a root
 # t = BinarySearchTree(6)
 # t.insert(5)
@@ -96,7 +212,15 @@ class BinarySearchTree:
 # t.insert(7)
 # t.queue.storage.get_cache()
 # t.insert(8)
-# t.queue.storage.get_cache()
+# t.queue.storage.get_cache() 
+
+# b = BinarySearchTree(5)
+# b.insert(10)
+# b.insert(4)
+# b.insert(2)
+# b.insert(7)
+# print(b.dft_print(10), "dft_print")
+# print(b.bft_print(10), "bft_print")
 
 #https://www.geeksforgeeks.org/binary-search-tree-data-structure/
 
